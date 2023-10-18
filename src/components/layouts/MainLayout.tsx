8be22d4b2fd684b7ff2db87ae2d8006ecb309f3d
ManuelaMarulandaA <mamarulandaa@unal.edu.co>
2023-10-18 18:47:35 -0500

    change header color

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -3,7 +3,7 @@ import type { FC, PropsWithChildren } from "react";
 const MainLayout: FC<PropsWithChildren> = ({ children }) => {
   return (
     <div className="flex h-full min-h-screen flex-col bg-slate-100">
-      <header>header goes here!</header>
+      <header className="bg-orange-600">header goes here!</header>
       <main className="flex-1">{children}</main>
       <footer>footer goes here!</footer>
     </div>
